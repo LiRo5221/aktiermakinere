a8c5c19d8c61da71fe3985a84f357dc95b84b0fe
LiRo5221 <thatfellaliro5221@gmail.com>
2022-03-14 18:17:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-
 var React = require('react');
-
-$ You can now view my-react-app-name in the browser.
-
-$ Local:            http://localhost:3000/
-$ On Your Network:  http://192.168.0.105:3000/
-
-$ Note that the development build is not optimized.
-$ To create a production build, use npm build.
-
-
-
 const StockSocket = require("stocksocket");
 
 StockSocket.addTicker("TSLA", stockPriceChanged);
