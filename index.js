2d12578945fd4e4dee5160b85820dd2edb530291
pizzanhugo <59748257+pizzanhugo@users.noreply.github.com>
2022-03-14 18:16:17 +0100

    maybe it works
    pizza

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+
+var React = require('react');
+
+$ You can now view my-react-app-name in the browser.
+
+$ Local:            http://localhost:3000/
+$ On Your Network:  http://192.168.0.105:3000/
+
+$ Note that the development build is not optimized.
+$ To create a production build, use npm build.
+
+
+
 const StockSocket = require("stocksocket");
 
 StockSocket.addTicker("TSLA", stockPriceChanged);
@@ -6,3 +19,4 @@ function stockPriceChanged(data) {
   //Choose what to do with your data as it comes in.
   console.log(data);
 }
+
