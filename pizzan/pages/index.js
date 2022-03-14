9eb84ac8c4db8c1791f90ee776df57041f43c96c
LiRo5221 <thatfellaliro5221@gmail.com>
2022-03-14 19:16:35 +0100

    e

--- pizzan/pages/index.js
+++ pizzan/pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <div>yooo</div>
+  return (
+    <body>
+      <label for="aktie">Aktie</label>
+      <input type="text" id="aktie" name="aktie"></input>
+    </body>
+
+  )
 };
