7c02ecfbd79ee3cb73fef3747bcf19a9f1144e99
pizzanhugo <59748257+pizzanhugo@users.noreply.github.com>
2022-03-14 19:30:22 +0100

    pizzan

--- pizzan/pages/index.js
+++ pizzan/pages/index.js
@@ -3,11 +3,77 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+   <body>
+     <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Document</title>
+        <link rel="stylesheet" href="app.css" />
+    </head>
     <body>
-      <label for="aktie">Aktie</label>
-      <input type="text" id="aktie" name="aktie"></input>
-      z
+        <div class="container">
+            <h1>&#x2728;Harry Potter Characters &#x2728;</h1>
+            <div id="searchWrapper">
+                <input
+                    type="text"
+                    name="searchBar"
+                    id="searchBar"
+                    placeholder="search for a character"
+                />
+            </div>
+            <ul id="charactersList"></ul>
+        </div>
+        <script src="app.js"></script>
     </body>
+</html>
+
+    
 
   )
 };
+const charactersList = document.getElementById('charactersList');
+const searchBar = document.getElementById('searchBar');
+let hpCharacters = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredCharacters = hpCharacters.filter((character) => {
+        return (
+            character.name.toLowerCase().includes(searchString) ||
+            character.house.toLowerCase().inc ludes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
+
+const loadCharacters = async () => {
+    try {
+        const res = await fetch('https://hp-api.herokuapp.com/api/characters');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
+
