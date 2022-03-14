65e504ac408341d351c38eace7432f79bdaea662
pizzanhugo <59748257+pizzanhugo@users.noreply.github.com>
2022-03-14 17:42:14 +0100

    this is fizz
    hello

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-console.log('hello world')
 
- log@console..((((((3=8)))))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for(var i = 0; i<100; i++)
+{
+if(i % 15 == 0)console.log("FizzBuzz");
+if(i % 5 == 0)console.log("Fizz");
+if(i % 3 == 0)console.log("Buzz");
+else console.log(i);
+
+}
